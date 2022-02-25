1a185fbe73804d7f988fa61014765edd54a88c50
Goutai Phan <sb.phanquocthai@gmail.com>
2022-02-26 03:55:40 +0700

    commit

--- javascript/baseScript.js
+++ javascript/baseScript.js
@@ -33,8 +33,8 @@ function setSize(object, marginDesktop, marginMobile) {
 
     window.scroll(0, 0);
     if (height < 600) {
-        document.body.style.minWidth = '100vw';
-        document.body.style.minHeight = '110vh';
+        // document.body.style.minWidth = '100vw';
+        document.body.style.minHeight = '50vh';
     } else {
         document.body.style.minWidth = '90vw';
         document.body.style.minHeight = '90vh';
