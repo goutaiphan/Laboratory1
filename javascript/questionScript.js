b67456d91cf1e0d33426d738ba48195e2acd66ea
Goutai Phan <sb.phanquocthai@gmail.com>
2022-03-13 11:06:07 +0700

    commit

--- javascript/questionScript.js
+++ javascript/questionScript.js
@@ -30,7 +30,7 @@ button.innerHTML = 'Xác nhận';
 
 let area = document.createElement('div');
 area.append(part, question, response, button);
-area.setRatio(-10, -50);
+area.setRatio(-10, -45);
 [part, question, children, button].setVisibility(false);
 document.body.append(area);
 
