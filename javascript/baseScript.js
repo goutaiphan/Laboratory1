0aae76460f32af7531ab27d419c149cd38b4de9d
Goutai Phan <sb.phanquocthai@gmail.com>
2022-03-14 04:34:37 +0700

    commit

--- javascript/baseScript.js
+++ javascript/baseScript.js
@@ -35,7 +35,7 @@ Object.prototype.setRatio = function () {
     } else {
         widthRatio = 1;
     }
-    this.style.padding = '25px 5vmax';
+    this.style.padding = '5vmax 25px';
     this.style.transform = `scale(${widthRatio})`;
     this.style.maxWidth = '450px';
 
