5b75ab548bdec047883cc38f4a3c61f1017149fe
Goutai Phan <sb.phanquocthai@gmail.com>
2022-03-14 03:46:36 +0700

    commit

--- javascript/answerScript.js
+++ javascript/answerScript.js
@@ -28,7 +28,7 @@ button.innerHTML = 'Tiếp tục';
 let area = document.createElement('div');
 area.className = 'area';
 area.append(result, answer, button);
-area.setRatio(30, -45);
+area.setRatio(30, 0);
 [...resultChildren, answer, button].setVisibility(false);
 document.body.append(area);
 
